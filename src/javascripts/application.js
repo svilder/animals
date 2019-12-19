e4b9792db9f666b681849bd034649b4f1ec2415c
Sandra Vilder <sandra.vilder@gmail.com>
2019-12-19 11:41:52 +0100

    fail JS

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -1,3 +1,25 @@
 import 'bootstrap';
 import 'bootstrap/scss/bootstrap.scss';
 import '../stylesheets/application.scss';
+
+
+const eyes = document.querySelectorAll('eye');
+const walk = 500; // 500px
+const body = document.querySelector('body');
+
+function moveEyes() {
+  eyes.classList.add('big');
+}
+
+
+window.addEventListener('click', moveEyes);
+
+//     left = ${yWalk * -1}px,
+//     bottom = ${xWalk * -1}px
+
+// eyes.style.right = `
+//     ${yWalk}px
+//   `;
+//   eyes.style.top = `
+//     ${xWalk}px
+//   `;
